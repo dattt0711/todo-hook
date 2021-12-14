2562afa4fc75cac74843fb348346e3bd58452244
dat <1801040060@s.hanu.edu.vn>
2021-12-15 06:01:40 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -24,28 +24,3 @@ function App(props) {
 }
 
 export default App;
-// class App extends Component {
-  //     constructor(props) {
-  //       super(props);
-  //       this.state = {
-  //         show: false
-  //       }
-  //   }
-    
-  //   isLogin =(data=undefined)=>{
-  //     if(data!=undefined){
-  //       this.setState({show:true});
-  //       alert(`Welcome ${data.username}, your email is ${data.email}`);
-  //     } 
-  //   }
-  //   render() {
-  //   return (
-  //     <div className="wrapper">
-  //     <div className="App">
-  //      {this.state.show || <Form isLogin={this.isLogin}></Form>}
-  //      {this.state.show && <TodoFeature/>}
-  //     </div>
-  //     </div>
-  //   );
-  //   }
-  // }
